1e6386cf748d194869d84ba97f755e8a9a9b313a
Biniam Berhe <96473632+BiniamBerhe@users.noreply.github.com>
2022-06-05 18:30:07 +0200

    Update App.py

--- App.py
+++ App.py
@@ -48,9 +48,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", threaded=True, port=port)
 
-    """
-    {
-    "idea1": "go and practice dancing",
-    "idea2": "find a dancing partner",
-    "idea3": "kill yourself"
-}"""
+  
